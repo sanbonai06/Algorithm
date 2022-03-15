edbcae086f2a766c7ecbc1bd2f2545d9089203aa
김민준 <sanbonai06@naver.com>
2022-03-16 00:42:17 +0900

    DP

--- DP/HomeworkFibonachi.cpp
+++ DP/HomeworkFibonachi.cpp
@@ -1,39 +1,8 @@
-// #include<iostream>
-// #include<cstring>
-// #define MAX 1000000
-// using namespace std;
-// long long dp[MAX];
-
-// long long fibo(int n) {
-//     if(n==1) return 1;
-//     if(n==2) return 1;
-
-//     if(dp[n] != -1) return dp[n];
-//     else if(dp[n] == -1) {
-//         return dp[n] = fibo(n-1) + fibo(n-2);
-//     }
-// }
-
-// long long fib_t(int n) {
-//     if(n==0) return 1;
-//     if(n==1) return 1;
-
-//     if(dp[n] != -1) return dp[n];
-//     else if(dp[n] == -1) {
-//         return dp[n]= fib_t(n-1) + fib_t(n-2) + 1;
-//     }
-// }
-
-// int main() {
-//     memset(dp, -1, sizeof(dp));
-//     int a;
-//     cin>>a;
-//     cout<<fib_t(a)<<endl;
-// }
-
 #include<stdio.h>
+#include<iostream>
 #include<string.h>
 #include<memory.h>
+using namespace std;
 #define N 209 //The 1000th Fibonacci number has 209 decimal digits.
 void print_digits(char d[N])
 {
@@ -49,37 +18,45 @@ void add_digits(char aa[N], char bb[N])
   int i=0;
   int carry=0;
   int j, s;
-  while(aa[i++]==0){  // after loop, i is the first "non-zero" place
-  printf("%d\n", i);
-  for(j=N-1;j>=i-1;j--) { // the sum can have non-zero at (i-1)th place
-    if( (s=carry+aa[j]+bb[j])>9 ) {         // 올림수 + aa + bb 가 10이 넘어가면
+//   while(aa[i++]==0)  // after loop, i is the first "non-zero" place
+//   {
+  for(j=N-1;j>=0;j--) { // the sum can have non-zero at (i-1)th place
+    if( (s=carry+aa[j]+bb[j])>9 ) {
       bb[j]=s-10;
       carry=1;
-    } else {
+    } 
+    else {
         bb[j]=s;
         carry=0; 
     }
-    }
-  } 
+  }
+//} 
 }
+
 int main()
 {
 // Write Your Progam Here!
-    char a[N];
-    char b[N];
-    char c[N];
-    bzero(a, sizeof(a));
-    bzero(b, sizeof(b));
-    bzero(c, sizeof(c));
+    char aa[N];
+    char bb[N];
+    char cc[N];
+    bzero(aa, sizeof(aa));
+    bzero(bb, sizeof(bb));
+    bzero(cc, sizeof(cc));
     int i=1;
-    a[0] = 0;   //n-2
-    b[0] = 1;   //n-1
-    for(i=2;i<N;i++) {
-        strcpy(c, a);
-        add_digits(b, a);
-        if(i == N-1) break;
-        strcpy(b, a);
-        strcpy(a, c);
+    bb[N-1] = 1;
+    aa[N-1] = 1;   //n-1 fib(1)
+    int d;
+    cin>>d;
+    for(i=3;i<=d;i++) {
+        strcpy(cc, aa);     //cc에 fib(n-1) 저장
+        add_digits(aa, bb);     //bb에 fib(n) 저장
+        if(i == d) break;
+        strcpy(aa, bb);         //aa에 fib(n-1)이 되는 것 저장
+        strcpy(bb, cc);         //bb에 fib(n-2)이 되는 것 저장
+        print_digits(aa);
+        printf("\n");
+        print_digits(bb);
+        printf("\n");
     }
-    print_digits(a);
+    // print_digits(bb);
 }
